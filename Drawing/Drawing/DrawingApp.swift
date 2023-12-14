6a680ba91105556cdef624c977348c646ce85eb2
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-14 05:31:27 +0200

    init project

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by OLEKSANDR ISAIEV on 14.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
